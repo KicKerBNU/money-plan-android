a82b173b91635a981cc901c9ef69af4ecf830a96
Everton Buzzi <evertonbuzzi@gmail.com>
2026-07-07 12:20:33 +0100

    feat(eb): update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.moneyplann.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.2"
+        versionCode = 7
+        versionName = "1.0.3"
         buildConfigField(
             "String",
             "API_BASE_URL",
